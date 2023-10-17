8cbb02d2ce3b8a1e6c53f82081d2899870dbbd46
sreeshmashaji <sreeshma3023@gmail.com>
2023-10-17 18:49:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+
+
+const app = express(); 
+const PORT = 4000; 
+app.use(express.json())
+
+
+app.get('/',(req,res)=>{
+    res.status(200).send("Welcome to sreeshma server")
+})
+app.listen(PORT, (error) =>{ 
+	if(!error) 
+		console.log("Server is Successfully Running , and App is listening on port "+ PORT) 
+	else
+		console.log("Error occurred, server can't start", error); 
+	} 
+); 
