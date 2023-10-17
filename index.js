e4a78d6e084583be47f90f99b413d9448337caf2
sreeshmashaji <sreeshma3023@gmail.com>
2023-10-17 21:28:01 +0530

    third  commit

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ app.use(express.json())
 app.get('/',(req,res)=>{
     res.status(200).send("Welcome to sreeshma server")
 })
+
+app.get('/another',(req,res)=>{
+    res.status(200).send("Welcome to another server")
+})
+
+
+
 app.listen(PORT, (error) =>{ 
 	if(!error) 
 		console.log("Server is Successfully Running , and App is listening to port "+ PORT) 
